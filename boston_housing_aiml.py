6ab2bd41b509292a09093844412543b62e592fd2
VivekNarula7 <viveknarula22@gmail.com>
2024-09-15 16:24:31 +0530

    Second Commit

--- boston_housing_aiml.py
+++ boston_housing_aiml.py
@@ -1,3 +1,4 @@
+# Importing all the necessary libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
+# We load are dataset from csv file into a pandas dataframe
 df = pd.read_csv('/home/vivek/Downloads/boston-housing/boston_housing_dataset/train.csv')
 print(df)
 
@@ -12,29 +14,27 @@ print(df)
 any_nulls = df.isnull().values.any()
 print(any_nulls)  # Returns True if there are any null values, False otherwise
 
-
+# We drop the 'ID' attribute as this is redundant and does not contribute to the predcition of our target variable
 df.drop('ID', axis=1, inplace=True)
 print(df)
 
+# We store the target variable into a separate variable and try to run our predciction operations on this
 X = df.drop('medv', axis=1)
-y = df['medv']
-
+y = df['medv'] # the target variable
 
 print(X,y)
 
+# Splitting the dataset into training and testing set using train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-
+# We train our Linear Regression model using sklearn.linear_model.LinearRegression() 
 model = LinearRegression()
-model.fit(X_train, y_train)
+model.fit(X_train, y_train) # fitting the model using the fit method
 
-# # Evaluate the model on training data
-# train_score = model.score(X_train, y_train)
-# print(f"Training R² Score: {train_score:.4f}")
-
-# Evaluate the model on test data
-test_score = model.score(X_test, y_test)
-print(f"Test R² Score: {test_score:.4f}")
+# Evaluating the model on the test set
+test_score = model.score(X_test, y_test) # The score function built into the LinearRegression module computes the R2 score
+print(f"Test R² Score: {test_score:.4f}") 
 
 y_pred = model.predict(X_test)
 
@@ -48,5 +48,4 @@ r2 = r2_score(y_test, y_pred)
 print(f"Mean Squared Error (MSE): {mse:.4f}")
 print(f"Root Mean Squared Error (RMSE): {rmse:.4f}")
 print(f"Mean Absolute Error (MAE): {mae:.4f}")
-
-
+print(f"R² Score (Test): {r2:.4f}")
